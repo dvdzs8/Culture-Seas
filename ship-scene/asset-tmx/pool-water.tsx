3a03bcd0bb8ccc628658c926f156128aec921eb2
Jessica Hsin <115275911+jhsin1@users.noreply.github.com>
2025-02-08 15:41:19 -0500

    Add files via upload

---
+++ ship-scene/asset-tmx/pool-water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pool-water" tilewidth="32" tileheight="32" tilecount="72" columns="8">
+ <image source="../asset-pngs/pool-water.png" width="268" height="306"/>
+</tileset>
---
+++ ship-scene/asset-tmx/real_ocean.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="real_ocean" tilewidth="32" tileheight="32" tilecount="255" columns="17">
+ <image source="../asset-pngs/real_ocean.PNG" width="560" height="502"/>
+</tileset>
